f3de864e94162fd37cd7a3c77578b2b9974a1ac4
Taylor Wilson <taylorjwilson1815@gmail.com>
2023-02-19 20:51:24 -0500

    Set up game where computer generates key and player guesses

--- mastermind.rb
+++ mastermind.rb
@@ -1,7 +1,7 @@
 
 
 COLORS = {r: "red", b:"blue", y:"yellow", g:"green", w:"white", o:"orange"}
-guess_arr = []
+ANS = Array.new(4,'r')
 
 def PlayGame
 
@@ -16,18 +16,27 @@ end
 
 
 def player_game
+    guess_arr = []
     #Computer generates random key
     password = generate_password
-    #player guess 4 colors (using key values)
-    guess = player_guess
-    #values are checked against computer key
-    #1. Check if values are included
     print(password)
-    guess_key = generate_checks(guess, password)
-    print(guess_key)
-    #2. Check if values in right spot
     #values added as next line of array
-
+    for i in 1..3
+        #player guess 4 colors (using key values)
+        guess = player_guess
+        guess_arr.push(guess.map {|x| x[0]})
+        display_game(guess_arr)
+        #values are checked against computer key
+        guess_key = generate_checks(guess, password)
+        print guess_key
+        print "\n\n"
+        if (guess_key == ANS)
+            print "You won!"
+            break
+        end
+        
+    end
+    print "You lost. Key was #{password} \n"
 end
 
 def generate_password
@@ -43,13 +52,21 @@ def generate_password
 end
 
 
-def display_game
+def display_game(matrix)
     
+    for i in 0..matrix.length - 1
+        for j in 0..3
+            print(matrix[i][j])
+            print " "
+        end
+        puts "\n"
+    end
+
 end
 
 
 def player_guess
-    print "Key is #{COLORS}. Input player guess: "
+    print "Key is #{COLORS}.\nInput player guess: "
     guess = gets.chomp.split(",").map{|x| COLORS[x.to_sym]}
     return guess
 end
@@ -57,12 +74,14 @@ end
 def generate_checks(guess, password)
     guess_key = Array.new
     uniq_guess = guess.uniq
+
+    #1. Check if values are included
     for i in 0..uniq_guess.length - 1
         if(password.include?(uniq_guess[i])) 
             guess_key.push('w')
         end
     end
-
+    #2. Check if values in right spot
     for i in 0..guess.length - 1
         if(guess[i] == password[i])
             guess_key.pop()
