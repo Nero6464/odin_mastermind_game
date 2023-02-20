f1f53e39196fa356b4bcf0e60134250a429e6a72
Taylor Wilson <taylorjwilson1815@gmail.com>
2023-02-19 19:39:12 -0500

    Generates player checks against computer key

--- mastermind.rb
+++ mastermind.rb
@@ -1,7 +1,7 @@
 
 
 COLORS = {r: "red", b:"blue", y:"yellow", g:"green", w:"white", o:"orange"}
-
+guess_arr = []
 
 def PlayGame
 
@@ -17,10 +17,15 @@ end
 
 def player_game
     #Computer generates random key
-    generate_password
+    password = generate_password
     #player guess 4 colors (using key values)
-
+    guess = player_guess
     #values are checked against computer key
+    #1. Check if values are included
+    print(password)
+    guess_key = generate_checks(guess, password)
+    print(guess_key)
+    #2. Check if values in right spot
     #values added as next line of array
 
 end
@@ -39,14 +44,32 @@ end
 
 
 def display_game
-    COLORS
+    
 end
 
 
 def player_guess
-    print "Input player guess: "
+    print "Key is #{COLORS}. Input player guess: "
     guess = gets.chomp.split(",").map{|x| COLORS[x.to_sym]}
     return guess
 end
 
-print player_guess
+def generate_checks(guess, password)
+    guess_key = Array.new
+    uniq_guess = guess.uniq
+    for i in 0..uniq_guess.length - 1
+        if(password.include?(uniq_guess[i])) 
+            guess_key.push('w')
+        end
+    end
+
+    for i in 0..guess.length - 1
+        if(guess[i] == password[i])
+            guess_key.pop()
+            guess_key.unshift('r')
+        end
+    end
+    return guess_key
+end
+
+player_game
